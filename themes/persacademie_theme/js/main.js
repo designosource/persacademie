380ff0d60b63df8d26d78ead788867ac4918a523
BenWitters <ben.witters@hotmail.com>
2017-05-21 20:07:48 +0200

    layout navigation

---
+++ themes/persacademie_theme/js/main.js
@@ -0,0 +1,20 @@
+console.log('test');
+(function ($) {
+    $(document).ready(function(){
+        
+        $('.hamburger-menu').on('click', function(){
+
+            $('.hamburger-menu-icon').toggleClass('is-clicked');
+            $('.header').toggleClass('menu-is-open');
+
+            if( $('.navbar-nav').hasClass('is-visible') ) {
+                $('.navbar-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+                });
+
+            } else {
+                $('.navbar-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+                });
+            }
+        });
+        
+    });}(jQuery));
